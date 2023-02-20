43c7a7966819e6c4b80f7c3ebe302cdb10765958
madhuamarnath <madhu.amarnath@gmail.com>
2023-02-20 23:01:11 +0530

    Delete IP Theory directory

--- IP Theory/JSON/JSON_Demo.js
+++
@@ -1,8 +0,0 @@
-console.log(JSON.stringify({ x: 5 }));        	    
-console.log(JSON.stringify(true));            	    
-console.log(JSON.stringify('foo'));           	    
-console.log(JSON.stringify(new Date(1950,11,12,23,49,00))); 
-
-const student = '{"regno":4100, "name":"Rahul", "cgpa":9.72, "dept":"CSE"}';
-const obj = JSON.parse(student);
-console.log(obj);
