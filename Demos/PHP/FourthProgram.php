a93b5cb74ca2a7590db3aaca66be77abbc0108c6
Madhu Amarnath <madhu.amarnath@gmail.com>
2023-04-10 12:53:14 +0530

    Update FourthProgram.php

--- Demos/PHP/FourthProgram.php
+++ Demos/PHP/FourthProgram.php
@@ -1,8 +1,9 @@
 <?php
-$name = "Rahul Dravid";    
+$name = "Rahul Dravid";   
 echo gettype($name)."\n";  
 $_100 = 36;                
 echo gettype($_100)."\n"; 
 $Avg = 52.12;              
-echo gettype($Avg);       
+echo gettype($Avg)."\n";    
+echo nl2br(gettype($name)."\n".gettype($_100)."\n".gettype($Avg)."\n");
 ?>
